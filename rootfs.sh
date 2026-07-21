3a31a8d05a5b5cd075cbbcee82acec1ebd7c160a
Stardust <57339966+StardustNews@users.noreply.github.com>
2026-07-21 17:58:09 +0900

    Change chmod command to use find for init.d files

--- rootfs.sh
+++ rootfs.sh
@@ -52,7 +52,7 @@ overlay() {
 
   rsync -a "$OVERLAY_WORKSPACE/" "$ROOTFS_WORKSPACE_MNT/"
   chown -R root:root "$ROOTFS_WORKSPACE_MNT/"
-  chmod 755 "$ROOTFS_WORKSPACE_MNT"/etc/init.d/*
+  find "$ROOTFS_WORKSPACE_MNT/etc/init.d" -maxdepth 1 -type f -exec chmod 755 {} +
   rm -rf "$OVERLAY_WORKSPACE"
 
   echo "Include /etc/ssh/sshd_config.d/*.conf" >> \
